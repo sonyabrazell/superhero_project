a2e9110bad14d5b97777f445448fb8ce48abeb32
Sonya Brazell <sonyabrazell@Sonyas-iMac.local>
2021-10-29 11:18:54 -0500

    completed code from videos and beginning second phase.

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Superhero
 
 
@@ -10,3 +11,23 @@ def index(request):
         'all_heroes' : all_heroes
     }
     return render(request,'superheroes/index.html', context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero' : single_hero
+    }
+    return render(request, 'superheroes/detail.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
